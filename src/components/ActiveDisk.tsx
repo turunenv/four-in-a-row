61bc535c98436e501d8997f4285215a25f34dd35
Verneri Turunen <turunenv@outlook.com>
2023-07-20 10:33:03 +0300

    ActiveDisk to follow cursor

--- src/components/ActiveDisk.tsx
+++ src/components/ActiveDisk.tsx
@@ -1,5 +1,33 @@
-export const ActiveDisk = () => {
+import './styles/ActiveDisk.css'
+
+import { useEffect, useState } from 'react';
+
+interface ActiveDiskProps {
+  playerOneIsNext: boolean;
+}
+
+export const ActiveDisk = ({ playerOneIsNext, winner }: ActiveDiskProps) => {
+  const [location, setLocation] = useState({ x: 400, y: 400 });
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      setLocation({ x: e.clientX - 28, y: e.clientY + 10 });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, [])
+
+  const style = {
+    top: location.y,
+    left: location.x,
+    backgroundColor: playerOneIsNext ? 'red' : 'yellow',
+  }
+  
+  
   return (
-    <div>ActiveDisk</div>
+    <div className="active-disk" style={style}>
+    </div>
   )
 }
