fe6b2db91d389abb12879e0f3746438763a5b7a9
Verneri Turunen <turunenv@outlook.com>
2023-07-20 10:33:33 +0300

    redefine global types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,17 @@
-export type GameColumn = (0 | 1 | 2)[];
+export type GameCellState = (0 | 1 | 2)
+export type GameColumn = GameCellState[];
 export type GameState = GameColumn[];
+
+
+
+export interface DiskIndeces {
+  col: number;
+  row: number;
+}
+
+export interface PlayerHasWon {
+  player: 1 | 2;
+  indices: DiskIndeces[];
+}
+
+export type HasPlayerWon = PlayerHasWon | false;
